2463ebb50277f2f491bd851c2c1d0a8034d8de60
Yassir Karroum <ukarroum17@gmail.com>
2024-03-10 11:25:27 +0100

    Added joystick control

--- control_vol.py
+++ control_vol.py
@@ -1,21 +1,42 @@
 import serial
 import alsaaudio
+from pymouse import PyMouse
 
-ser = serial.Serial('/dev/ttyUSB0')
+ser = serial.Serial('/dev/ttyUSB1')
 
 m = alsaaudio.Mixer()
 m.setmute(False)
 vol = m.getvolume()[0]
 
+mouse = PyMouse()
+
+def update_vol(new_vol):
+    global vol
+    if abs(new_vol - vol) > 1:
+        m.setvolume(int(new_vol))
+        vol = new_vol
+
+
+def update_pos(x, y):
+    mouse.move(mouse.position()[0] - int(y/70), mouse.position()[1] + int(x/70))
+
+
+def click():
+
+    mouse.click(mouse.position()[0], mouse.position()[1])
+
+
 while True:
-    print(m.getvolume())
     line = ser.readline()
 
     try:
-        new_vol = (int(line.decode().strip()) / 1024) * 100
-        if abs(new_vol - vol) > 1:
-            m.setvolume(int(new_vol))
-            vol = new_vol
+        match line.decode().split():
+            case ['VOL', new_vol]:
+                update_vol(new_vol)
+            case ['JOYSTICK', x, y]:
+                update_pos(int(x) - 520, int(y) - 520)
+            case ['CLICK']:
+                click()
 
     except UnicodeDecodeError:
         pass
--- control_vol_arduino/control_vol_arduino.ino
+++ control_vol_arduino/control_vol_arduino.ino
@@ -1,15 +1,44 @@
 int volPin = A1;
 int vol = 0;
 
+int joyBtn = 2;
+int joyXPin = A2;
+int joyYPin = A3;
+
+int x = 0;
+int y = 0;
+
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode(joyXPin, INPUT);
+  pinMode(joyYPin, INPUT);
+  pinMode(joyBtn, INPUT_PULLUP);
+  
 }
 
 void loop()
 {
-  vol = analogRead(A1);
+  vol = analogRead(volPin);
 
-  Serial.print(vol);
-  Serial.print('\n');
+  //Serial.print("VOL ");
+  //Serial.print(vol);
+  //Serial.print("\n");
+
+  x = analogRead(joyXPin);
+  y = analogRead(joyYPin);
+  
+  Serial.print("JOYSTICK ");
+  Serial.print(x);
+  Serial.print(" ");
+  Serial.print(y);
+  Serial.print("\n");
+
+  if(!digitalRead(joyBtn))
+  {
+    Serial.print("CLICK\n");
+    delay(100);
+  }
+
 }
