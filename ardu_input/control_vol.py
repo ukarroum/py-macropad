3357b79fad0aa0431f9d99d09c6c02989aa9de34
Yassir Karroum <ukarroum17@gmail.com>
2025-03-04 20:27:47 +0100

    Removed old mouse code

--- ardu_input/control_vol.py
+++ ardu_input/control_vol.py
@@ -1,6 +1,5 @@
 import serial
 import alsaaudio
-from pymouse import PyMouse
 
 ser = serial.Serial('/dev/ttyUSB1')
 
@@ -8,22 +7,12 @@ m = alsaaudio.Mixer()
 m.setmute(False)
 vol = m.getvolume()[0]
 
-mouse = PyMouse()
 
 def update_vol(new_vol):
     global vol
     if abs(new_vol - vol) > 1:
         m.setvolume(int(new_vol))
         vol = new_vol
-
-
-def update_pos(x, y):
-    mouse.move(mouse.position()[0] - int(y/70), mouse.position()[1] + int(x/70))
-
-
-def click():
-
-    mouse.click(mouse.position()[0], mouse.position()[1])
 
 
 while True:
@@ -33,10 +22,6 @@ while True:
         match line.decode().split():
             case ['VOL', new_vol]:
                 update_vol(new_vol)
-            case ['JOYSTICK', x, y]:
-                update_pos(int(x) - 520, int(y) - 520)
-            case ['CLICK']:
-                click()
 
     except UnicodeDecodeError:
         pass
