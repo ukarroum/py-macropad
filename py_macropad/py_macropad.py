f639dd2ae276923c77c3170e776672fbce1ecb03
Yassir Karroum <ukarroum17@gmail.com>
2025-03-22 19:05:54 +0100

    Added button code and renamed project

--- py_macropad/py_macropad.py
+++ py_macropad/py_macropad.py
@@ -1,17 +1,19 @@
+from subprocess import Popen
+
 import serial
 import alsaaudio
 
-ser = serial.Serial('/dev/ttyUSB1')
+ser = serial.Serial('/dev/ttyUSB0')
 
 m = alsaaudio.Mixer()
 m.setmute(False)
 vol = m.getvolume()[0]
 
 
-def update_vol(new_vol):
+def update_vol(new_vol: int):
     global vol
     if abs(new_vol - vol) > 1:
-        m.setvolume(int(new_vol))
+        m.setvolume(int((new_vol / 1024) * 100))
         vol = new_vol
 
 
@@ -21,7 +23,9 @@ while True:
     try:
         match line.decode().split():
             case ['VOL', new_vol]:
-                update_vol(new_vol)
+                update_vol(int(new_vol))
+            case ['BTN']:
+                Popen(["ghostty"])
 
     except UnicodeDecodeError:
         pass
--- py_macropad_hardware/py_macropad_hardware.ino
+++ py_macropad_hardware/py_macropad_hardware.ino
@@ -1,10 +1,15 @@
+// Vol
 int volPin = A1;
 int vol = 0;
 
+// Btn
+int btnPin = 2;
 
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode(btnPin, INPUT);
 }
 
 void loop()
@@ -14,4 +19,11 @@ void loop()
   Serial.print("VOL ");
   Serial.print(vol);
   Serial.print("\n");
+
+  if(digitalRead(btnPin) == HIGH)
+  {
+    Serial.print("BTN\n");
+    delay(200);
+  }
+    
 }
